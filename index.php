e4a12d5b0d4d46dfda46f6a44c4b5c884c0016c9
qiuzhuowei <53961021+qiuzhuowei@users.noreply.github.com>
2020-10-06 15:12:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,46 @@
-<?php
-//载入qrcode类库
-include "./phpqrcode.php";
+<html>
+  <head>
+    <link rel="shortcut icon" href="favicon.ico">
+        <meta name="viewport" content="width=device-width">
+        <meta name="keywords" content="VIP视频解析,CSS,JavaScript">
+        <meta name="description" content="VIP视频解析">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>手艺舍-VIP视频解析</title>
+            <style>
+    body{color:#cfcfcf;background:#009688;text-align:center;font-size:18px;}
+    a{text-decoration:none;font-size:16px;}
+    a:link {color:#cfcfcf}
+    a:visited {color:#cfcfcf}
+    a:hover {color:blue} 
+    a:active {color:blue}
+    p{color:#000000;}
+    h2{color:#ff1f1f}
+    .v{width:180px;height:30px;}
+    .anniu{height:30px;width:80px}
+        </style>
+  </head>
+  <body>
+    <br>
+    <h2>VIP视频解析</h2>
+	  
+	<p>复制你想要观看的VIP视频链接</p>
+	<p>将复制的链接粘贴在下方</p>
+	<p>点击观看视频</p>
+	<p>左上角可切换播放线路</p>
 
-//取得GET参数
-$text       = isset($_GET["text"]) ? $_GET["text"] : ''; //二维码内容
-$errorLevel = isset($_GET["e"]) ? $_GET["e"] : 'L'; //容错级别 默认L
-$PointSize  = isset($_GET["p"]) ? $_GET["p"] : '5'; //二维码尺寸 默认5
-$margin     = isset($_GET["m"]) ? $_GET["m"] : '2'; //二维码白边框尺寸 默认2
-//去掉下方注释，可以检测二维码内容是否包含某字段 ，防止盗链。
-/*
-$isok 		= strstr($text, "luckymoke.cn"); //要检测的内容根据自己需求改
-if(!$isok){
-	echo "403 Forbidden";
-	exit; //停止继续执行
-}
-*/
-//二维码生成函数
-function getqrcode($value,$errorCorrectionLevel,$matrixPointSize,$margin) {
-    QRcode::png($value, false, $errorCorrectionLevel, $matrixPointSize, $margin);
-}
-getqrcode($text, $errorLevel, $PointSize, $margin);
-?>
+      
+    <form action="http://www.shouyishe.cf/ys/?v=" method="get" >
+      <input type="text" name="v" class="v" placeholder="输入视频链接或影片名"><br><br>
+      <input type="submit" value="观看视频" class="anniu" target="_blank"/>
+    </form>
+	
+	<br>
+	<img src="https://shouyishe.com/wp-content/uploads/2020/02/Snipaste_2020-02-10_11-07-18.png" width="200" height="240">
+	
+    <br><br><br>
+    <div>
+        <a href="https://shouyishe.com/">手艺舍</font></a>&nbsp;|&nbsp;
+        <a href="/yy" target="_blank">音乐下载</font></a>&nbsp;|&nbsp;
+        <a href="/wy" target="_blank">个人网页生成</font></a><br></div>
+  </body>
+</html>
